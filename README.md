498c261574fc49c3e0ff7f62065be69b064d9f66
Felipe Alfonso González <f.alfonso.go@gmail.com>
2018-01-24 01:25:58 -0300

    v1.0.0 - Test Zweicom

---
+++ README.md
@@ -0,0 +1,34 @@
+# Prueba Zweicom
+
+PRUEBA:
+
+1.- Crear GitHub repo si no tiene
+
+2.-Escribir servicio http que:
+ - Acepte un número como consulta
+ - Dado el número recibido, retorne el término n y n-1 de la sucesión de Fibonacci.
+
+3.- En el lenguaje que prefiera
+4.- Escribir tests para su código (que estime convenientes)
+
+Entregar la ruta del repo GitHub y todo su desarrollo y test ahí. 
+
+
+* Repositorio debe contener README que describa el desarrollo, dependencias, como ejecutar y como utilizar el servicio.
+
+
+## Quick Start
+
+1. `sudo npm install`
+2. `node server.js`
+3. Open <http://localhost:3000>
+
+
+
+## Authors and contribs.
+Developed by Felipe Alfonso Gonzalez // Software Developer - felipe.dev.engr.js@gmail.com 
+
+
+## Copyright and license
+ 
+Copyright JS Foundation and other contributors, http://js.foundation under [the GPU/GPL](LICENSE).
---
+++ pseudoco.js
@@ -0,0 +1,51 @@
+// 
+
+// Pseucodigo para test de prueba 
+// tests de tipos de algoritmos a usar : probando 
+// esperando sea el correcto, 
+
+
+// intento nro.1 
+
+// Incio
+// nvar  = num o n (consulta)
+// enter = nvar;
+// Si (n = nvar)
+//      return nvar; and return n-1 (sucession de fibonacci)
+//      si no
+//      null
+// fin.-
+
+
+// intento nro2 
+
+Inicio
+escriba un numero _________ = (__primero__)
+suma = primernum = segundonum (creando un for i++)
+primer = segundo 
+sumasupertota = seungonum
+print ___sumasupertota___
+fin 
+
+
+// para tener en cuenta:
+// en la sucession de fibonacci la suma es la suma de los dos anteriores en cada termino, ej.:
+// 1+1 = 2
+// 2+1  = 3
+// 3+2 = 5
+// 5 + 3 = 8 
+// 8+5 = 13
+// 13 + 8 = 21
+
+
+
+// possiblemente investigare sibre alguna libreria para la ejecucion de la sucession de fibonacci
+
+// servicio HTTP, con interfaz web a travez handlebars
+
+
+
+
+
+// 
+
